47494c871f7347977d9016ace5c2f079458ef7f7
TheRawKing <harshit.anadd@gmail.com>
2025-12-17 17:04:26 +0530

    [Harshit Anandd] : added second program

---
+++ gcr-codebase/program-prerequisites/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+public class AddTwoNumbers {
+	public static void main(String args[]) {
+		int a, b, sum;
+		a=225;
+		b=335;
+		sum = a+b;
+		System.out.println("The sum of " + a + " and " + b + " is : " + sum);
+	}
+}
