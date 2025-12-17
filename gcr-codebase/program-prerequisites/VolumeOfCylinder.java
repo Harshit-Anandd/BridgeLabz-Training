760ed0ef03c48ae6d145957a0280f1f1832071ec
TheRawKing <harshit.anadd@gmail.com>
2025-12-17 17:05:51 +0530

    [Harshit Anandd] : added fifth program

---
+++ gcr-codebase/program-prerequisites/VolumeOfCylinder.java
@@ -0,0 +1,7 @@
+public class VolumeOfCylinder {
+    public static void main (String args[]) {
+        int radius = 7;
+        int height = 10;
+        System.out.println("Volume of Cylinder = " + (Math.PI*radius*radius*height));
+    }
+}
