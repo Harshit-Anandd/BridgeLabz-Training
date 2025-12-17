08d90d447e004d3b817d86f7a6529b65783c73d1
TheRawKing <harshit.anadd@gmail.com>
2025-12-17 17:05:24 +0530

    [Harshit Anandd] : added fourth program

---
+++ gcr-codebase/program-prerequisites/AreaOfCircle.java
@@ -0,0 +1,6 @@
+public class AreaOfCircle {
+    public static void main (String args[]) {
+        int radius = 25;
+        System.out.println("Area of Circle = " + (Math.PI*radius*radius));
+    }
+}
