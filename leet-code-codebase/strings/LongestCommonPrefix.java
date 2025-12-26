c21e0e0150f6f9dcfce8b68a5ca0253a438aa73d
TheRawKing <harshit.anadd@gmail.com>
2025-12-27 02:04:04 +0530

    [Harshit Anandd] : added LongestCommonPrefix.java in strings

---
+++ leet-code-codebase/strings/LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+// Solution for LeetCode #14
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // Edge case: empty array
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        // Take first string as reference
+        String prefix = strs[0];
+        
+        // Compare prefix with each string
+        for (int i = 1; i < strs.length; i++) {
+            // Reduce prefix until it matches current string
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                
+                // If prefix becomes empty, no common prefix
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        
+        return prefix;
+    }
+}
