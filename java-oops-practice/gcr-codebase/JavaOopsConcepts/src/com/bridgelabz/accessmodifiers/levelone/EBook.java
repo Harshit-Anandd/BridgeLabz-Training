a283426beed0b644964af824152685207708a8ce
TheRawKing <harshit.anadd@gmail.com>
2025-12-29 04:09:39 +0530

    [Harshit Anandd] : added EBook.java in level one

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/accessmodifiers/levelone/EBook.java
@@ -0,0 +1,26 @@
+// Create class EBook that extends Book to demonstrate inheritance and access modifiers.
+
+package com.bridgelabz.accessmodifiers.levelone;
+
+public class EBook extends Book {
+    // Constructor to initialize EBook by calling parent class constructor
+    public EBook(String isbn, String title, String author) {
+        super(isbn, title, author);
+    }
+
+    // Method to display EBook information using different access levels
+    public void displayEBookInfo() {
+        // Access protected field from parent class
+        System.out.println("EBook Title: " + title);
+        // Access public field from parent class
+        System.out.println("ISBN: " + isbn);
+        // Access private field through public getter method
+        System.out.println("Author: " + getAuthor());
+    }
+
+    // Main method to test EBook class
+    public static void main(String[] args) {
+        EBook ebook = new EBook("978-3-16", "Digital Learning", "Dr. Bob");
+        ebook.displayEBookInfo();
+    }
+}
