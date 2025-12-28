ffc46cc1af2f33a907db4bb76df2dd60481e3caa
TheRawKing <harshit.anadd@gmail.com>
2025-12-29 04:03:09 +0530

    [Harshit Anandd] : added Book.java in level one

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructors/levelone/Book.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.constructors.levelone;
+
+//Create class Book to demonstrate default and parameterized constructors.
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    // Default Constructor
+    public Book() {
+        this.title = "Unknown Title";
+        this.author = "Unknown Author";
+        this.price = 0.0;
+    }
+
+    // Parameterized Constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Display book details
+    public void displayDetails() {
+        System.out.println("Title: " + this.title + ", Author: " + this.author + ", Price: $" + this.price);
+    }
+
+    public static void main(String[] args) {
+
+        // Using Default Constructor
+        Book defaultBook = new Book();
+        defaultBook.displayDetails();
+
+        // Using Parameterized Constructor
+        Book paramBook = new Book("Effective Java", "Joshua Bloch", 45.00);
+        paramBook.displayDetails();
+    }
+}
