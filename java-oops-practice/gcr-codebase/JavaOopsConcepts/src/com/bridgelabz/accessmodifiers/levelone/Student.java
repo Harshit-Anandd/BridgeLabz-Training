b9b7faefdb55f2b7a6f503c62bc527d826ba518c
TheRawKing <harshit.anadd@gmail.com>
2025-12-29 04:09:50 +0530

    [Harshit Anandd] : added Student.java in level one

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/accessmodifiers/levelone/Student.java
@@ -0,0 +1,26 @@
+//Create class Student to demonstrate access modifiers with attributes and getters/setters.
+
+package com.bridgelabz.accessmodifiers.levelone;
+
+public class Student {
+    public int rollNumber;
+    protected String name;
+    private double cgpa;
+
+    // Constructor to initialize Student attributes
+    public Student(int rollNumber, String name, double cgpa) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    // Getter method to retrieve CGPA
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    // Setter method to update CGPA
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+}
