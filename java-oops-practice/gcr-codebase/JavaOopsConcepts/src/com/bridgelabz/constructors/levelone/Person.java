53a4a09ba2f45daf244a0664b6f48735dfaa3aa9
TheRawKing <harshit.anadd@gmail.com>
2025-12-29 04:09:32 +0530

    [Harshit Anandd] : added Person.java in level one

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructors/levelone/Person.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.constructors.levelone;
+
+//Create class Person to demonstrate parameterized and copy constructors.
+public class Person {
+    private String name;
+    private int age;
+
+    // Parameterized Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Copy Constructor
+    public Person(Person other) {
+        this.name = other.name;
+        this.age = other.age;
+    }
+
+    // Display person details
+    public void display() {
+        System.out.println("Name: " + this.name + ", Age: " + this.age);
+    }
+
+    public static void main(String[] args) {
+
+        // Create original person
+        Person p1 = new Person("Alice", 25);
+        System.out.println("Original Person:");
+        p1.display();
+
+        // Create cloned person using copy constructor
+        Person p2 = new Person(p1);
+        System.out.println("Cloned Person:");
+        p2.display();
+    }
+}
