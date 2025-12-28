b7666ea20a93bdff08509b62e70808ef8ebd643c
TheRawKing <harshit.anadd@gmail.com>
2025-12-29 04:09:36 +0530

    [Harshit Anandd] : added Book.java in level one

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/accessmodifiers/levelone/Book.java
@@ -0,0 +1,26 @@
+//Create class Book to demonstrate access modifiers with book attributes.
+
+package com.bridgelabz.accessmodifiers.levelone;
+
+public class Book {
+    public String isbn;
+    protected String title;
+    private String author;
+
+    // Constructor to initialize Book attributes
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getter method to retrieve author
+    public String getAuthor() {
+        return author;
+    }
+
+    // Setter method to update author
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
