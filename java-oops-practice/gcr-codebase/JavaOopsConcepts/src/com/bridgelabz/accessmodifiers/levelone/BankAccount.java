e18c841d680f9974f3aca882d4d637f3566038d3
TheRawKing <harshit.anadd@gmail.com>
2025-12-29 04:09:34 +0530

    [Harshit Anandd] : added BankAccount.java in level one

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/accessmodifiers/levelone/BankAccount.java
@@ -0,0 +1,28 @@
+//Create class BankAccount to demonstrate access modifiers with banking operations.
+
+package com.bridgelabz.accessmodifiers.levelone;
+
+public class BankAccount {
+    public String accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    // Constructor to initialize BankAccount attributes
+    public BankAccount(String accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    // Getter method to retrieve account balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Method to deposit amount into account with validation
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+}
