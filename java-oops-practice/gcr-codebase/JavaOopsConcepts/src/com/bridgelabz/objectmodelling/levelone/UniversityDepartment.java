b7e4b27c0a910d350da658e547f5bbfcb45f0932
TheRawKing <harshit.anadd@gmail.com>
2025-12-31 13:28:27 +0530

    [Harshit Anandd] : added UniversityDepartment.java in level one

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/objectmodelling/levelone/UniversityDepartment.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+public class UniversityDepartment {
+    private String name;
+    public UniversityDepartment(String name) { 
+    	this.name = name;
+    }
+    public String getName() { 
+    	return name;
+    }
+}
