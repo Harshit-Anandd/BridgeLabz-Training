39100c2c1d2091129fb34ee2fe36001c7e4b69f9
TheRawKing <harshit.anadd@gmail.com>
2025-12-31 13:27:58 +0530

    [Harshit Anandd] : added Employee.java in level one

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/objectmodelling/levelone/Employee.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+public class Employee {
+    private String name;
+    public Employee(String name) { 
+    	this.name = name;
+    }
+    public String toString() {
+    	return name;
+    }
+}
