9cdc24ef48bb4c4276e0f295b0b5ce6cf2fd0e2c
TheRawKing <harshit.anadd@gmail.com>
2026-01-02 01:14:58 +0530

    [Harshit Anandd] : added Dog.java in assisted problems

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/assistedproblems/Dog.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.inheritance.assistedproblems;
+
+public class Dog extends Animal {
+    public Dog(String name, int age) { super(name, age); }
+    
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " barks.");
+    }
+}
