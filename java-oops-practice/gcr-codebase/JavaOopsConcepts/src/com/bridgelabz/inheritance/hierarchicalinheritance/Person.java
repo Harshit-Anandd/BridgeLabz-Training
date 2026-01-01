29ddaa10931cfe4d31ac5fd208d9afecbc1ffe21
TheRawKing <harshit.anadd@gmail.com>
2026-01-02 01:15:27 +0530

    [Harshit Anandd] : added Person.java in hierarchical inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/hierarchicalinheritance/Person.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.inheritance.hierarchicalinheritance;
+
+public class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayRole() {
+        System.out.println("Role: General Person");
+    }
+}
