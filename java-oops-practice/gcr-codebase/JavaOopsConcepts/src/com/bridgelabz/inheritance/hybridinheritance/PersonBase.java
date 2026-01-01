e020935966d8d0eec1119f845a74f83527b5e87c
TheRawKing <harshit.anadd@gmail.com>
2026-01-02 01:15:48 +0530

    [Harshit Anandd] : added PersonBase.java in hybrid inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/hybridinheritance/PersonBase.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.inheritance.hybridinheritance;
+
+public class PersonBase {
+    protected String name;
+    protected int id;
+
+    public PersonBase(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
