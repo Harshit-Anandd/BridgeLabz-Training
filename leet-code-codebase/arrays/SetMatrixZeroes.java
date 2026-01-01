3fa0c3b35a9d675992a086f147195dc68dbd10c8
TheRawKing <harshit.anadd@gmail.com>
2026-01-01 13:37:02 +0530

    [Harshit Anandd] : added SetMatrixZeroes.java in arrays

---
+++ leet-code-codebase/arrays/SetMatrixZeroes.java
@@ -0,0 +1,60 @@
+// Solution for LeetCode #73
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        // Check if first row has zero
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        
+        // Check if first column has zero
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        
+        // Use first row and column as markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // Mark row
+                    matrix[0][j] = 0; // Mark column
+                }
+            }
+        }
+        
+        // Set matrix elements to zero based on markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        // Set first row to zero if needed
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        // Set first column to zero if needed
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
