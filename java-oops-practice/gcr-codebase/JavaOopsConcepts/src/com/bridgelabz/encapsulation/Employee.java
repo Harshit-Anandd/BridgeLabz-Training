9ede8479bd2e6b91c4ae8e48d64331b23b4c9316
TheRawKing <harshit.anadd@gmail.com>
2026-01-02 01:13:30 +0530

    [Harshit Anandd] : added Employee.java in encapsulation

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/encapsulation/Employee.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.encapsulation;
+
+public abstract class Employee {
+    private int employeeId;
+    private String name;
+    private double baseSalary;
+
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    // Encapsulation: Getters and Setters
+    public int getEmployeeId() { return employeeId; }
+    public String getName() { return name; }
+    public double getBaseSalary() { return baseSalary; }
+
+    // Abstract method to be implemented by subclasses
+    public abstract double calculateSalary();
+
+    // Concrete method
+    public void displayDetails() {
+        System.out.println("ID: " + employeeId + ", Name: " + name);
+    }
+}
