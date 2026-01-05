31baef5e4ff850a0b88c96b19bca753a7c71d1dc
TheRawKing <harshit.anadd@gmail.com>
2026-01-05 16:30:12 +0530

    [Harshit Anandd] : added LCSMain.java in longest consecutive sequence

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/hashmap/longestconsecutivesequence/LCSMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hashmap.longestconsecutivesequence;
+
+public class LCSMain {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 9, 3, 10, 4, 20, 2};
+        System.out.println("Length of longest consecutive sequence: " + LongestConsecutiveSequence.findLongestConseqSubseq(arr));
+    }
+    
+}
