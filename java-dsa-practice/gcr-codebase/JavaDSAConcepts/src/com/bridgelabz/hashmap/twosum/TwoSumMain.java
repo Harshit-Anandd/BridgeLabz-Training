e6b80d3f52493ab9d08635cd1ed39ca60997a2c8
TheRawKing <harshit.anadd@gmail.com>
2026-01-05 16:30:25 +0530

    [Harshit Anandd] : added TwoSumMain.java in two sum

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/hashmap/twosum/TwoSumMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.hashmap.twosum;
+
+import java.util.Arrays;
+
+public class TwoSumMain {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = TwoSum.twoSum(nums, target);
+        System.out.println("Indices: " + Arrays.toString(result));
+    }
+    
+}
