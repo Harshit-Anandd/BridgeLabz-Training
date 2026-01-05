f5432ac736cbfd9245c0695075828bb9da0ff1fa
TheRawKing <harshit.anadd@gmail.com>
2026-01-05 17:19:11 +0530

    [Harshit Anandd] : added CourseActions.java in Campus Connect

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day03/campusconnect/CourseActions.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day03.campusconnect;
+
+// 1. Interface: Actions available for academic courses
+public interface CourseActions {
+    void enrollCourse(String courseName);
+    void dropCourse(String courseName);
+}
