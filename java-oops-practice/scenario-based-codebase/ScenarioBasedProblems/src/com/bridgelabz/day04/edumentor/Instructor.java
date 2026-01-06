59f312f6b384a46af1fdcb1a99351121890ba12f
TheRawKing <harshit.anadd@gmail.com>
2026-01-07 00:58:47 +0530

    [Harshit Anandd] : added Instructor.java in Edu-Mentor

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day04/edumentor/Instructor.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day04.edumentor;
+
+public class Instructor extends User {
+    public Instructor(String name, String email, String userId) {
+        super(name, email, userId);
+    }
+}
