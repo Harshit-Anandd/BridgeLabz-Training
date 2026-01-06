abe9571e3671452a20d016bd1ce0d6e446d0a480
TheRawKing <harshit.anadd@gmail.com>
2026-01-07 00:58:49 +0530

    [Harshit Anandd] : added Learner.java in Edu-Mentor

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day04/edumentor/Learner.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day04.edumentor;
+
+public class Learner extends User {
+    public Learner(String name, String email, String userId) {
+        super(name, email, userId);
+    }
+}
