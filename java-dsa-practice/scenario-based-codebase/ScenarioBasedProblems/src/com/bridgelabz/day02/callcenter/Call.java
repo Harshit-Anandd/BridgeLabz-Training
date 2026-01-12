092536726f40b71373a9ef5ddbc87e86ca5a3447
TheRawKing <harshit.anadd@gmail.com>
2026-01-12 15:11:04 +0530

    [Harshit Anandd] : added Call.java in Call Center

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day02/callcenter/Call.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day02.callcenter;
+
+public class Call {
+    String customerId;
+    boolean isVip;
+
+    Call(String customerId, boolean isVip) {
+        this.customerId = customerId;
+        this.isVip = isVip;
+    }
+}
