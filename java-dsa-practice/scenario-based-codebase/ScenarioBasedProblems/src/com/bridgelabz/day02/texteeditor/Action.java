b6766eec715f121253b25022eef47dc03e306eb5
TheRawKing <harshit.anadd@gmail.com>
2026-01-12 15:11:12 +0530

    [Harshit Anandd] : added Action.java in Text Editor

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day02/texteeditor/Action.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day02.texteeditor;
+
+public class Action {
+    String type;
+    String content;
+
+    Action(String type, String content) {
+        this.type = type;
+        this.content = content;
+    }
+}
