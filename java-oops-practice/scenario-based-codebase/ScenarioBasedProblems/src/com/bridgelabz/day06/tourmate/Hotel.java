c6660a60c035499ea542ef90434e27964cbab9aa
TheRawKing <harshit.anadd@gmail.com>
2026-01-27 00:23:45 +0530

    [Harshit Anandd] : added Hotel.java in TourMate

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day06/tourmate/Hotel.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day06.tourmate;
+
+// 2. Helper Classes for Association
+public class Hotel {
+    String name;
+    double cost;
+    public Hotel(String name, double cost) { 
+	this.name = name; 
+	this.cost = cost; 
+	}
+}
