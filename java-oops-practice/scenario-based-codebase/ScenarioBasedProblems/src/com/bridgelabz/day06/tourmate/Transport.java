361bd07568ac5887226a6c8e3d69b47c53bc392e
TheRawKing <harshit.anadd@gmail.com>
2026-01-27 00:23:54 +0530

    [Harshit Anandd] : added Transport.java in TourMate

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day06/tourmate/Transport.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day06.tourmate;
+
+// 2. Helper Classes for Association
+public class Transport {
+    String type;
+    double cost;
+    public Transport(String type, double cost) { 
+	this.type = type; 
+	this.cost = cost; 
+	}
+}
