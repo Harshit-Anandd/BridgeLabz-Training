fa5c11d55642465f98ca3019faee046c204c1b28
TheRawKing <harshit.anadd@gmail.com>
2026-01-27 00:24:33 +0530

    [Harshit Anandd] : added Instructor.java in SkillForge

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day07/skillforge/Instructor.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.day07.skillforge;
+
+// 3. Derived Users
+public class Instructor extends User {
+    public Instructor(String name, String email, String id) { 
+		super(name, email, id);
+	}
+}
