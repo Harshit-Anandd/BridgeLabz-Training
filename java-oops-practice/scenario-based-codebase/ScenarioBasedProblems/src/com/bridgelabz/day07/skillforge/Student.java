040613f32ab6d02518e5e5f6f80cd7ea93e9d11f
TheRawKing <harshit.anadd@gmail.com>
2026-01-27 00:24:37 +0530

    [Harshit Anandd] : added Student.java in SkillForge

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day07/skillforge/Student.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.day07.skillforge;
+
+// 3. Derived Users
+public class Student extends User {
+    public Student(String name, String email, String id) { 
+		super(name, email, id);
+	}
+}
