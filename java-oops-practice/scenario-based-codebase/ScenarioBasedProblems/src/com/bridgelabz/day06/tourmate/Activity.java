23feb7549ba0a87636ecbbcac0705fbc7ec0f0f2
TheRawKing <harshit.anadd@gmail.com>
2026-01-27 00:23:40 +0530

    [Harshit Anandd] : added Activity.java in TourMate

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day06/tourmate/Activity.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day06.tourmate;
+
+// 2. Helper Classes for Association
+public class Activity {
+    String name;
+    double cost;
+    public Activity(String name, double cost) { 
+	this.name = name; 
+	this.cost = cost; 
+	}
+}
