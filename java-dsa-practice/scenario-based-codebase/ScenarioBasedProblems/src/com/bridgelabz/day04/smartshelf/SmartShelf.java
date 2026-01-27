3371c1ff046ce998799170cc08c0441ae657abe5
TheRawKing <harshit.anadd@gmail.com>
2026-01-28 01:27:04 +0530

    [Harshit Anandd] : added SmartShelf.java in Smart Shelf

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day04/smartshelf/SmartShelf.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.day04.smartshelf;
+
+public class SmartShelf {
+	
+	public static void insertionSort(Book[] books, int n) {
+
+        for (int i = 1; i < n; i++) {
+            Book key = books[i];
+            int j = i - 1;
+
+            while (j >= 0 && books[j].title.compareToIgnoreCase(key.title) > 0) {
+                books[j + 1] = books[j];
+                j--;
+            }
+            books[j + 1] = key;
+        }
+    }
+}
