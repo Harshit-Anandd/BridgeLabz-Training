31767de80e46f59dd361ef14260810ee0a911b3b
TheRawKing <harshit.anadd@gmail.com>
2026-01-28 01:26:20 +0530

    [Harshit Anandd] : added VehicleNode.java in Traffic Manager

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day02/trafficmanager/VehicleNode.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day02.trafficmanager;
+
+public class VehicleNode {
+    String vehicleNumber;
+    VehicleNode next;
+
+    VehicleNode(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+}
