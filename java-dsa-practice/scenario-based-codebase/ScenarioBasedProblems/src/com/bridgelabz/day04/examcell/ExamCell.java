89b863bb99f2a5c607eeaf8ebd395b11c3bcf735
TheRawKing <harshit.anadd@gmail.com>
2026-01-28 01:26:46 +0530

    [Harshit Anandd] : added ExamCell.java in Exam Cell

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day04/examcell/ExamCell.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.day04.examcell;
+
+public class ExamCell {
+
+    public static void merge(Student[] arr, int l, int m, int r) {
+
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        Student[] L = new Student[n1];
+        Student[] R = new Student[n2];
+
+        for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i].score >= R[j].score) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    public static void mergeSort(Student[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+    
+}
