afa244aac3d09481a7f574b7010703a903c1d718
TheRawKing <harshit.anadd@gmail.com>
2026-01-28 01:26:54 +0530

    [Harshit Anandd] : added HospitalQueue.java in Hospital Queue

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day04/hospitalqueue/HospitalQueue.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.day04.hospitalqueue;
+
+public class HospitalQueue {
+	
+	public static void bubbleSort(Patient[] patients, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (patients[j].criticality < patients[j + 1].criticality) {
+                    Patient temp = patients[j];
+                    patients[j] = patients[j + 1];
+                    patients[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
