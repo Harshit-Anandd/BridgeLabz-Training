881d30731246e4672309a0a95a592bf87fb165c4
TheRawKing <harshit.anadd@gmail.com>
2026-01-28 01:26:35 +0530

    [Harshit Anandd] : added Customer.java in Smart Checkout

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day03/smartcheckout/Customer.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day03.smartcheckout;
+
+public class Customer {
+    String name;
+    String[] items;
+
+    Customer(String name, String[] items) {
+        this.name = name;
+        this.items = items;
+    }
+}
