4521742420b8e30020b2e491267add2ae97f62e3
TheRawKing <harshit.anadd@gmail.com>
2026-01-29 00:30:55 +0530

    [Harshit Anandd] : added BorrowedBook.java in Smart Library

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day06/smartlibrary/BorrowedBook.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day06.smartlibrary;
+
+public class BorrowedBook {
+    String title;
+
+    BorrowedBook(String title) {
+        this.title = title;
+    }
+
+    public String toString() {
+        return title;
+    }
+}
