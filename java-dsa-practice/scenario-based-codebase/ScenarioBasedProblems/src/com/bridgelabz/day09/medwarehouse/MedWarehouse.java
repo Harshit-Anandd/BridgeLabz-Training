d119ed2d75fd917ab6c8575d5abdaef1cee73c1a
TheRawKing <harshit.anadd@gmail.com>
2026-01-29 00:31:46 +0530

    [Harshit Anandd] : added MedWarehouse.java in Med Warehouse

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day09/medwarehouse/MedWarehouse.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.day09.medwarehouse;
+import java.util.Scanner;
+
+public class MedWarehouse {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Medicine[] meds = new Medicine[50];
+        int count = 0;
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n1.Add Medicine  2.Sort  3.Display  4.Exit");
+            int ch = sc.nextInt();
+            sc.nextLine();
+
+            switch (ch) {
+                case 1:
+                    System.out.print("Medicine name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Expiry (YYYYMMDD): ");
+                    int expiry = sc.nextInt();
+                    meds[count++] = new Medicine(name, expiry);
+                    break;
+
+                case 2:
+                    MedicineSorter.mergeSort(meds, 0, count - 1);
+                    break;
+
+                case 3:
+                    for (int i = 0; i < count; i++)
+                        System.out.println(meds[i]);
+                    break;
+
+                case 4:
+                    running = false;
+            }
+        }
+        sc.close();
+    }
+}
