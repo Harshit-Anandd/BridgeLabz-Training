4d59b88dd0ec6b7ab2a0fe1461bfaafe0d58e368
TheRawKing <harshit.anadd@gmail.com>
2026-01-29 00:30:52 +0530

    [Harshit Anandd] : added IceCreamSorter.java in Ice Cream Rush

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day06/icecreamrush/IceCreamSorter.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.day06.icecreamrush;
+
+public class IceCreamSorter {
+
+    static void bubbleSort(IceCreamFlavor[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j].soldCount < arr[j + 1].soldCount) {
+                    IceCreamFlavor temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
