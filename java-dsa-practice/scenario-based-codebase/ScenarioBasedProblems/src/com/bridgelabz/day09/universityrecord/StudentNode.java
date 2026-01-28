f92e4ba06af12a0e2c385992bb910236e1512870
TheRawKing <harshit.anadd@gmail.com>
2026-01-29 00:32:20 +0530

    [Harshit Anandd] : added StudentNode.java in University Record

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day09/universityrecord/StudentNode.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day09.universityrecord;
+
+public class StudentNode {
+    int rollNo;
+    String name;
+    StudentNode left, right;
+
+    StudentNode(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+}
