cbf14469eb07fb47cea9ca758c24a330fc69e455
TheRawKing <harshit.anadd@gmail.com>
2026-01-29 00:30:18 +0530

    [Harshit Anandd] : added CropMonitor.java in Crop Monitor

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bridgelabz/day05/cropmonitor/CropMonitor.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.day05.cropmonitor;
+
+public class CropMonitor {
+
+    public static int partition(SensorReading[] arr, int low, int high) {
+
+        long pivot = arr[high].timestamp;
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j].timestamp < pivot) {
+                i++;
+                SensorReading temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        SensorReading temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void quickSort(SensorReading[] arr, int low, int high) {
+
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+}
