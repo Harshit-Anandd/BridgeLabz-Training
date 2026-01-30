a832e9067c85056025ba1744d4ebb2ad2db9b88a
TheRawKing <harshit.anadd@gmail.com>
2026-01-31 01:54:24 +0530

    [Harshit Anandd] : added Electronics.java in Warehouse Management

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/generics/warehousemanagement/Electronics.java
@@ -0,0 +1,7 @@
+package com.generics.warehousemanagement;
+
+public class Electronics extends WarehouseItem {
+    public Electronics(String name, double price) {
+        super(name, price);
+    }
+}
