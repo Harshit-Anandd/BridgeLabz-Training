b94cfc27c1c37ea16ff2285e238ff3d14d01a2f4
TheRawKing <harshit.anadd@gmail.com>
2026-01-31 01:52:11 +0530

    [Harshit Anandd] : added AgeValidator.java in Age Check

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/exceptions/agecheck/AgeValidator.java
@@ -0,0 +1,11 @@
+package com.exceptions.agecheck;
+
+public class AgeValidator {
+    public void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        } else {
+            System.out.println("Access granted!");
+        }
+    }
+}
