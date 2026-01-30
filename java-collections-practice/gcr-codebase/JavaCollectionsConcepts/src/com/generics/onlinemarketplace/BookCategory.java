289aca3b2943f8b7c5112c8826e6e868c76c4bd3
TheRawKing <harshit.anadd@gmail.com>
2026-01-31 01:53:38 +0530

    [Harshit Anandd] : added BookCategory.java in Online Marketplace

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/generics/onlinemarketplace/BookCategory.java
@@ -0,0 +1,8 @@
+package com.generics.onlinemarketplace;
+
+public class BookCategory extends ProductCategory {
+    @Override
+    public String getCategoryName() { 
+    	return "Books"; 
+    }
+}
