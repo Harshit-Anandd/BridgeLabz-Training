a77306e2682faa136ddcdaeb359784f93d43e7f8
TheRawKing <harshit.anadd@gmail.com>
2026-01-31 01:54:16 +0530

    [Harshit Anandd] : added ExamCourse.java in University Management

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/generics/universitymanagement/ExamCourse.java
@@ -0,0 +1,8 @@
+package com.generics.universitymanagement;
+
+public class ExamCourse extends CourseType {
+    @Override
+    public String getEvaluationMethod() { 
+    	return "Written Exam"; 
+    }
+}
