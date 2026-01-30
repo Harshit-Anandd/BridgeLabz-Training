0dc29e05ced7b440832794a257e38d3a997702ff
TheRawKing <harshit.anadd@gmail.com>
2026-01-31 01:54:08 +0530

    [Harshit Anandd] : added AssignmentCourse.java in University Management

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/generics/universitymanagement/AssignmentCourse.java
@@ -0,0 +1,8 @@
+package com.generics.universitymanagement;
+
+public class AssignmentCourse extends CourseType {
+    @Override
+    public String getEvaluationMethod() { 
+    	return "Project Submission"; 
+    }
+}
