bb69cdbd997673e1cc7ff90bcdf73c8451aae89a
TheRawKing <harshit.anadd@gmail.com>
2026-02-06 23:30:55 +0530

    [Harshit Anandd] : added Claim.java in Insurance Claim

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/streams/insuranceclaim/Claim.java
@@ -0,0 +1,20 @@
+package com.streams.insuranceclaim;
+
+public class Claim {
+
+	private String type;
+	private double amount;
+
+	public Claim(String type, double amount) {
+		this.type = type;
+		this.amount = amount;
+	}
+
+	public String getType() { 
+		return type; 
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+}
