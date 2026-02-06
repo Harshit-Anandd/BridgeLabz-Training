5413a769cae4a82d38f39fe8322b0eba1c878972
TheRawKing <harshit.anadd@gmail.com>
2026-02-06 23:29:20 +0530

    [Harshit Anandd] : added ElectricVehicle.java in Vehicle Dashboard

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/functionalinterfaces/vehicledashboard/ElectricVehicle.java
@@ -0,0 +1,9 @@
+package com.functionalinterfaces.vehicledashboard;
+
+//Electric vehicle adds battery display
+public class ElectricVehicle implements Vehicle {
+
+	public void displayBattery() {
+		System.out.println("Battery level displayed.");
+	}
+}
