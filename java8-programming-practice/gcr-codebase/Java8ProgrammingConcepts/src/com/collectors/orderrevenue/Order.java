11c29ec284cbcd01b0ba6c1b71ee109b4678f31f
TheRawKing <harshit.anadd@gmail.com>
2026-02-06 23:27:47 +0530

    [Harshit Anandd] : added Order.java in Order Revenue

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/collectors/orderrevenue/Order.java
@@ -0,0 +1,21 @@
+package com.collectors.orderrevenue;
+
+//Order model
+public class Order {
+
+	private String customer;
+	private double amount;
+
+	public Order(String customer, double amount) {
+		this.customer = customer;
+		this.amount = amount;
+	}
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+}
