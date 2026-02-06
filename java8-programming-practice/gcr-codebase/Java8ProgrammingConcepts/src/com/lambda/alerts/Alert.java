91517d8ef8d8659d4e597a9f65782ef01a1351a6
TheRawKing <harshit.anadd@gmail.com>
2026-02-06 23:29:44 +0530

    [Harshit Anandd] : added Alert.java in Lambda Alerts

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/lambda/alerts/Alert.java
@@ -0,0 +1,17 @@
+package com.lambda.alerts;
+
+public class Alert {
+	private String type;
+
+	public Alert(String type) {
+		this.type = type;
+	}
+
+	public String getType() { 
+		return type; 
+	}
+
+	public String toString() { 
+		return type; 
+	}
+}
