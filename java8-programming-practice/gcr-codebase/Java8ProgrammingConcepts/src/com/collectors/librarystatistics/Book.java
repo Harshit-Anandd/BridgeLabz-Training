2b0325d84228d5f60ccc6cdc87c5e669a3e530df
TheRawKing <harshit.anadd@gmail.com>
2026-02-06 23:27:41 +0530

    [Harshit Anandd] : added Book.java in Library Statistics

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/collectors/librarystatistics/Book.java
@@ -0,0 +1,21 @@
+package com.collectors.librarystatistics;
+
+//Book model
+public class Book {
+
+	private String genre;
+	private int pages;
+
+	public Book(String genre, int pages) {
+		this.genre = genre;
+		this.pages = pages;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public int getPages() {
+		return pages;
+	}
+}
