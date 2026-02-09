a420b779a8f517070600742fc89dbcd3b60ffca5
TheRawKing <harshit.anadd@gmail.com>
2026-02-10 00:56:01 +0530

    [Harshit Anandd] : added DatabaseConnection.java in JUnit Main

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/junit/main/DatabaseConnection.java
@@ -0,0 +1,10 @@
+package com.junit.main;
+
+public class DatabaseConnection {
+	public void connect() {
+		System.out.println("Database Connected");
+	}
+	public void disconnect() {
+		System.out.println("Database Disconnected");
+	}
+}
