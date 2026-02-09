27cb8addfbc323dfe0610bcddc8c3613fd461c32
TheRawKing <harshit.anadd@gmail.com>
2026-02-10 00:57:58 +0530

    [Harshit Anandd] : added Author.java in Retrieve Annotations

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/reflections/retrieveannotations/Author.java
@@ -0,0 +1,9 @@
+package com.reflections.retrieveannotations;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Author {
+	String name();
+}
