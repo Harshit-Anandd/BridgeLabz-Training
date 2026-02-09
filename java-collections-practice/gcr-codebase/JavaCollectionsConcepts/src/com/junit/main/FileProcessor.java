1134161b31cb395ffe2f23bdad8f06625aebee65
TheRawKing <harshit.anadd@gmail.com>
2026-02-10 00:56:07 +0530

    [Harshit Anandd] : added FileProcessor.java in JUnit Main

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/junit/main/FileProcessor.java
@@ -0,0 +1,14 @@
+package com.junit.main;
+
+import java.io.*;
+import java.nio.file.*;
+
+public class FileProcessor {
+	public void writeToFile(String filename, String content) throws IOException {
+		Files.write(Paths.get(filename), content.getBytes());
+	}
+
+	public String readFromFile(String filename) throws IOException {
+		return new String(Files.readAllBytes(Paths.get(filename)));
+	}
+}
