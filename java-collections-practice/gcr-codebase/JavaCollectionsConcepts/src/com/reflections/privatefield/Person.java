65f9ed7d496c208164ddd9382ad7aa915c9552a2
TheRawKing <harshit.anadd@gmail.com>
2026-02-10 00:57:33 +0530

    [Harshit Anandd] : added Person.java in Private Field

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/reflections/privatefield/Person.java
@@ -0,0 +1,14 @@
+package com.reflections.privatefield;
+
+public class Person {
+	private int age;
+
+	public Person(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [age=" + age + "]";
+	}
+}
