31b3cf435a714c256dfbe1f1957da9d5b0e26946
Anand K <harshit.anandd@gmail.com>
2026-02-23 01:42:10 +0530

    [Harshit Anandd] : added AnswerSheet.java in Exam Scanner

---
+++ java-collections-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/day01/examscanner/AnswerSheet.java
@@ -0,0 +1,22 @@
+package com.day01.examscanner;
+
+import java.util.List;
+
+public class AnswerSheet<T> {
+
+    private String studentName;
+    private List<T> answers;
+
+    public AnswerSheet(String studentName, List<T> answers) {
+        this.studentName = studentName;
+        this.answers = answers;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public List<T> getAnswers() {
+        return answers;
+    }
+}
