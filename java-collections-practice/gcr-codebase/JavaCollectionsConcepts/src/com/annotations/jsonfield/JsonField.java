a3bdf1d655b50c4c44789524e2f615a6df523fef
Anand K <harshit.anandd@gmail.com>
2026-02-23 01:39:58 +0530

    [Harshit Anandd] : added JsonField.java in Json Field

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/annotations/jsonfield/JsonField.java
@@ -0,0 +1,8 @@
+package com.annotations.jsonfield;
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface JsonField {
+    String name();
+}
