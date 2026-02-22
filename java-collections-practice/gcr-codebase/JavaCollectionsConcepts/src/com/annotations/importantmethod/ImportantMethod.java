a69c9c2c54e026a0770348d56d93fb13553654a3
Anand K <harshit.anandd@gmail.com>
2026-02-23 01:39:53 +0530

    [Harshit Anandd] : added ImportantMethod.java in Important Method

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/annotations/importantmethod/ImportantMethod.java
@@ -0,0 +1,8 @@
+package com.annotations.importantmethod;
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ImportantMethod {
+    String level() default "HIGH";
+}
