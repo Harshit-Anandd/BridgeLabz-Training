75893d07224665055f9c93f1b1951426befa55bf
Anand K <harshit.anandd@gmail.com>
2026-02-23 21:54:27 +0530

    [Harshit Anandd] : added IplMatch.java in IPL Module

---
+++ java-io-streams-practice/gcr-codebase/JavaIOStreamsConcepts/src/main/java/com/jsonfilehandling/ipl/IplMatch.java
@@ -0,0 +1,11 @@
+
+package com.jsonfilehandling.ipl;
+import java.util.Map;
+public class IplMatch {
+    public int match_id;
+    public String team1;
+    public String team2;
+    public Map<String,Integer> score;
+    public String winner;
+    public String player_of_match;
+}
