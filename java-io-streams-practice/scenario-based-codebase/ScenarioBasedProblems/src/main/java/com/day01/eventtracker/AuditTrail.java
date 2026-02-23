b1fd451b2a653aec7d8b0226782bb5242c9d9f4b
Anand K <harshit.anandd@gmail.com>
2026-02-23 21:54:30 +0530

    [Harshit Anandd] : added AuditTrail.java in Event Tracker

---
+++ java-io-streams-practice/scenario-based-codebase/ScenarioBasedProblems/src/main/java/com/day01/eventtracker/AuditTrail.java
@@ -0,0 +1,8 @@
+package com.day01.eventtracker;
+
+import java.lang.annotation.*;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AuditTrail {
+    String action();
+}
