b89af42b063e732ef9687e0357e77feccf50e6b7
Anand K <harshit.anandd@gmail.com>
2026-02-24 02:12:32 +0530

    [Harshit Anandd] : added LibraryApp.java in smartuniversitylibrary

---
+++ java-design-pattern-practice/gcr-codebase/JavaDesignPatternConcepts/src/com/designpattern/smartuniversitylibrary/LibraryApp.java
@@ -0,0 +1,7 @@
+package com.designpattern.smartuniversitylibrary;
+public class LibraryApp {
+    public static void main(String[] args) {
+        LibraryController controller = new LibraryController();
+        controller.start();
+    }
+}
